0444776038e13a50f64de692208a6bafc27d6794
Muhammet190 <31957047+Muhammet190@users.noreply.github.com>
2018-03-02 11:57:42 +0100

    Add files via upload

--- stopwatch.cpp
+++ stopwatch.cpp
@@ -10,3 +10,32 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include <stdlib.h>
+#include <stdio.h>
+#include "sorting_algorithms.h"
+#include "stopwatch.h"
+#include 
+
+void start_stopwatch()
+{
+  int msec = 0, trigger = 10; /* 10ms */
+  clock_t before = clock();
+
+  do {
+    clock_t difference = clock() - before;
+    msec = difference * 1000 / CLOCKS_PER_SEC;
+    iterations++;
+  } while ( msec < trigger );
+
+  printf("Time taken %d seconds %d milliseconds (%d iterations)\n",
+    msec/1000, msec%1000, iterations);
+}
+float elapsed_time()
+{
+
+}
+void stopwatch()
+{
+
+
+}
